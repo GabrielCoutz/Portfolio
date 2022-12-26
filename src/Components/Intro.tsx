26d7ead278a518346a396c874b47ed432e5e5bfb
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2022-12-26 19:10:15 -0300

    Intro completa

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "../../styles/Intro.module.css";
+import MouseSvg from "./UiElements/MouseSvg";
 
 const Intro = () => {
   return (
-    <div className={`${styles.intro} container`}>
-      <section>intro</section>
+    <div className="container">
+      <section className={styles.intro}>
+        <h1 className="title">
+          Hi, i am <span className="destaque">Christopher Hansen</span>
+        </h1>
+        <span className={styles.subTitle}>Dev Front-end | UI Designer</span>
+        <div className={styles.mouse}>
+          <MouseSvg />
+        </div>
+      </section>
     </div>
   );
 };
