644fee907169e48fb802c00ed2ebb7393c44e49e
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2022-12-27 14:04:41 -0300

    Cards improvement

--- src/Components/Projetos.tsx
+++ src/Components/Projetos.tsx
@@ -8,6 +8,7 @@ interface CardModel {
   subtitle: string;
   image: string;
   date: string;
+  description: string;
 }
 
 const variants: Variants = {
@@ -24,11 +25,9 @@ const variants: Variants = {
     animation: "none",
     transition: {
       type: "spring",
-      bounce: 0.7,
       duration: 0.7,
-      delayChildren: 0.3,
+      delayChildren: 0.1,
     },
-
     scale: 1.1,
   },
 };
@@ -36,13 +35,39 @@ const variants: Variants = {
 const spanVariant: Variants = {
   default: {
     opacity: 0,
+    transform: "translateY(200px)",
   },
   hover: {
     opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0,
+      delayChildren: 0.1,
+    },
+    transform: "translateY(0px)",
   },
 };
 
-const Card = ({ image, title, subtitle, date }: CardModel) => {
+const imageVariant: Variants = {
+  default: {
+    filter: "none",
+  },
+  hover: {
+    filter: "blur(2px)",
+    scale: 0.95,
+  },
+};
+
+const listVariant: Variants = {
+  default: {
+    transform: "translateY(-200px)",
+  },
+  hover: {
+    transform: "translateY(0px)",
+  },
+};
+
+const Card = ({ image, title, subtitle, date, description }: CardModel) => {
   return (
     <motion.div
       className={styles.card}
@@ -55,13 +80,17 @@ const Card = ({ image, title, subtitle, date }: CardModel) => {
       <h1 className={styles.title}>{title}</h1>
       <h3 className={styles.date}>{date}</h3>
       <div className={styles.details}>
-        <motion.span variants={spanVariant}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et
-          reiciendis rerum repellendus magnam eligendi corrupti architecto ad
-          tempore dolorum? Facilis iure saepe ut sapiente adipisci minus harum!
-          Est, quam.
-        </motion.span>
-        <Image src={image} width={300} height={300} alt={title} />
+        <motion.span variants={spanVariant}>{description}</motion.span>
+        <motion.ul className={styles.tecnologias} variants={listVariant}>
+          <motion.li>React</motion.li>
+          <motion.li>Next Js</motion.li>
+          <motion.li>GrapQL</motion.li>
+          <motion.li>Typescript</motion.li>
+          <motion.li>UX/UX</motion.li>
+        </motion.ul>
+        <motion.div variants={imageVariant}>
+          <Image src={image} width={300} height={300} alt={title} />
+        </motion.div>
       </div>
     </motion.div>
   );
@@ -76,12 +105,14 @@ const Projetos = () => {
         subtitle="Front end & UI Design"
         date="2022"
         image={require("../public/All-star.webp")}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
       />
       <Card
         title="Kairos"
         subtitle="Front/Back end & UI/UX Design"
         date="2021 - 2022"
         image={require("../public/Nike-Air-Force-2.webp")}
+        description="Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
       />
     </div>
   );
