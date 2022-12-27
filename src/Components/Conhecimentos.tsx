324f3ef062ba8cb105eda8d84e00271c4830d23f
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2022-12-27 00:02:25 -0300

    Conhecimentos completo

--- src/Components/Conhecimentos.tsx
+++ src/Components/Conhecimentos.tsx
@@ -1,42 +1,33 @@
 import React from "react";
 import styles from "../styles/Conhecimentos.module.css";
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { motion } from "framer-motion";
 import ReactSvg from "./UiElements/ReactSvg";
 import NextSvg from "./UiElements/NextSvg";
 import GraphqlSvg from "./UiElements/GraphqlSvg";
-
-const variants = {
-  default: { scale: 1 },
-  active: {
-    scale: 1.2,
-    transition: {
-      type: "spring",
-      bounce: 0.7,
-      velocity: 30,
-    },
-  },
-};
+import CssSvg from "./UiElements/CssSvg";
+import GitSvg from "./UiElements/GitSvg";
+import HtmlSvg from "./UiElements/HtmlSvg";
+import JavascriptSvg from "./UiElements/JavascriptSvg";
+import SassSvg from "./UiElements/SassSvg";
+import TypescriptSvg from "./UiElements/TypescriptSvg";
+import VueSvg from "./UiElements/VueSvg";
 
 const Conhecimentos = () => {
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 1.2]);
   return (
     <div className={styles.wrapperConhecimentos}>
-      <section className={`${styles.conhecimentos} container`}>
+      <section className={`${styles.conhecimentos}`}>
         <h1>What i know?</h1>
-        <motion.div
-          style={{ scale }}
-          variants={variants}
-          initial="default"
-          whileHover="active"
-          className={styles.bubble}
-        >
+        <motion.div>
+          <ReactSvg />
           <NextSvg />
+          <GraphqlSvg />
+          <CssSvg />
+          <GitSvg />
+          <HtmlSvg />
+          <JavascriptSvg />
+          <SassSvg />
+          <TypescriptSvg />
+          <VueSvg />
         </motion.div>
       </section>
     </div>
