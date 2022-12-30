d5ec51a9a02c2710735cf7085e51b1c8f828d980
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2022-12-29 21:08:46 -0300

    Animação na intro

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,13 +1,35 @@
-import React from "react";
 import styles from "../styles/Intro.module.css";
 import MouseSvg from "./UiElements/Svg/MouseSvg";
+import { motion, Variants } from "framer-motion";
+
+export const introVariant: Variants = {
+  default: {
+    scale: 0.4,
+    opacity: 0,
+  },
+  active: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.5,
+      delayChildren: 1,
+    },
+  },
+};
 
 const Intro = () => {
   return (
     <section className={`${styles.intro} container`}>
-      <h1 className="title">
-        Olá, sou <span className="destaque">Gabriel Coutinho</span>
-      </h1>
+      <motion.h1
+        className="title"
+        variants={introVariant}
+        initial="default"
+        whileInView="active"
+      >
+        Hi, i am
+        <span className="destaque"> Chistopher Hansen</span>
+      </motion.h1>
       <span className={styles.subTitle}>Dev Front-end | UI Designer</span>
       <div className={styles.mouse}>
         <MouseSvg />
