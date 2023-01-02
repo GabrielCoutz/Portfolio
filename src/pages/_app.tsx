8dfce0b60a2e1ef81abf33490162f3daa3d6cf6e
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-02 09:05:45 -0300

    Adição de inércia no scroll com js nativo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
-import Teste from "../Components/teste";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,9 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Portfolio G. Coutinho</title>
         <link rel="icon" href="../public/Logo2.ico" />
       </Head>
-      <Teste>
-        <Component {...pageProps} />
-      </Teste>
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,70 @@
+import React, { RefObject } from "react";
 import Conhecimentos from "../Components/Conhecimentos";
 import Contato from "../Components/Contato";
 import Header from "../Components/Header";
 import Intro from "../Components/Intro";
 import Projetos from "../Components/Projetos";
 
+let sx = 0, // For scroll positions
+  sy = 0;
+let dx = sx, // For container positions
+  dy = sy;
+
+function easeScroll() {
+  sx = window.pageXOffset;
+  sy = window.pageYOffset;
+}
+
+function render(main: HTMLDivElement) {
+  function li(a: number, b: number, n: number) {
+    return (1 - n) * a + n * b;
+  }
+  //We calculate our container position by linear interpolation method
+  dx = li(dx, sx, 0.07);
+  dy = li(dy, sy, 0.07);
+
+  dx = Math.floor(dx * 100) / 100;
+  dy = Math.floor(dy * 100) / 100;
+
+  main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+  window.requestAnimationFrame(() => render(main));
+}
+
+function teste(mainObj: RefObject<HTMLDivElement>) {
+  const body = document.querySelector("body");
+  const main = mainObj.current;
+  if (!main || !body) return;
+
+  body.style.height = main.clientHeight + "px";
+  main.style.position = "fixed";
+  main.style.top = "0";
+  main.style.left = "0";
+
+  return { body, main };
+}
+
 export default function Home() {
+  const elementRef = React.useRef<HTMLDivElement>(null);
+  React.useEffect(() => {
+    const references = teste(elementRef);
+    if (!references || !references.main) return;
+
+    window.addEventListener("scroll", easeScroll);
+    window.requestAnimationFrame(() => render(references.main));
+
+    return () => window.removeEventListener("scroll", easeScroll);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Intro />
-      <Conhecimentos />
-      <Projetos />
-      <Contato />
+      <div ref={elementRef}>
+        <Header />
+        <Intro />
+        <Conhecimentos />
+        <Projetos />
+        <Contato />
+      </div>
     </>
   );
 }
