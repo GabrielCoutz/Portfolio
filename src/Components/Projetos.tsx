305214e0a94db8a27ba3dcbe7ebc5665079fb5c4
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-02 19:37:06 -0300

    Responsividade

--- src/Components/Projetos.tsx
+++ src/Components/Projetos.tsx
@@ -15,29 +15,31 @@ const Projetos = () => {
       <div className={`${styles.projetos} container`}>
         <Subtitle>Feitos com muito carinho</Subtitle>
         <h1 className="title">Alguns dos meus projetos</h1>
-        <Card
-          title="Essencial Calçados"
-          subtitle="Front end & UI Design"
-          date="2022"
-          image={require("../public/image.webp")}
-          description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
-          tecnologies={[
-            "React",
-            "Next.js",
-            "Typescript",
-            "GrapqQL",
-            "HTML",
-            "CSS",
-          ]}
-        />
-        <Card
-          title="Kairos"
-          subtitle="Front/Back end & UI/UX Design"
-          date="2021 - 2022"
-          image={require("../public/img-projeto.png")}
-          description="Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
-          tecnologies={["HTML", "CSS", "JavaScript", "PHP"]}
-        />
+        <div className={styles.cards}>
+          <Card
+            title="Essencial Calçados"
+            subtitle="Front end & UI Design"
+            date="2022"
+            image={require("../public/image.webp")}
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
+            tecnologies={[
+              "React",
+              "Next.js",
+              "Typescript",
+              "GrapqQL",
+              "HTML",
+              "CSS",
+            ]}
+          />
+          <Card
+            title="Kairos"
+            subtitle="Front/Back end & UI/UX Design"
+            date="2021 - 2022"
+            image={require("../public/img-projeto.png")}
+            description="Voluptatem blanditiis quod nobis amet dolorum itaque at mollitia animi recusandae, necessitatibus a exercitationem minus, reiciendis quisquam nam et assumenda suscipit unde?"
+            tecnologies={["HTML", "CSS", "JavaScript", "PHP"]}
+          />
+        </div>
       </div>
     </motion.div>
   );
